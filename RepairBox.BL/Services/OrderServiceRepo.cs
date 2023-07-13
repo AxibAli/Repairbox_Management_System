e0dd89c46959e7f22240aab768cf184079b8e737
Khizir Farrukh <khizirfarrukh@outlook.com>
2023-07-13 22:13:38 +0500

    Completed Order Listings

--- RepairBox.BL/Services/OrderServiceRepo.cs
+++ RepairBox.BL/Services/OrderServiceRepo.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BitMiracle.LibTiff.Classic;
+using Microsoft.EntityFrameworkCore;
 using RepairBox.BL.DTOs.Order;
 using RepairBox.BL.DTOs.Stripe;
 using RepairBox.BL.DTOs.User;
@@ -9,6 +10,7 @@ using RepairBox.DAL;
 using RepairBox.DAL.Entities;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +21,7 @@ namespace RepairBox.BL.Services
     {
         CalculateOrderAmountDTO CalculateOrder(GetOrderChargesDTO model);
         Task CreateOrder(AddOrderDTO model);
-        PaginationModel GetOrderList(int pageNo, string? query);
+        PaginationModel GetOrderList(int pageNo, DisplayOrderFiltersDTO? orderFilters);
     }
 
     public class OrderServiceRepo : IOrderServiceRepo
@@ -108,7 +110,7 @@ namespace RepairBox.BL.Services
                 throw new Exception(ex.Message);
             }
         }
-        public PaginationModel GetOrderList(int pageNo, string? query)
+        public PaginationModel GetOrderList(int pageNo, DisplayOrderFiltersDTO? orderFilters)
         {
             var orderList = _context.Orders
                 .Join(_context.Models, order => order.ModelId, model => model.Id, (order, model) => new { Order = order, Model = model })
@@ -133,20 +135,153 @@ namespace RepairBox.BL.Services
                     PriorityId = result.Priority.Id,
                     PriorityName = result.Priority.Name,
                     StatusId = result.Status.Id,
-                    StatusName = result.Status.Name
+                    StatusName = result.Status.Name,
+                    WarrantyStatus = result.Order.WarrantyStatus,
+                    CreatedAt = result.Order.CreatedAt,
+                    AmountPaid = 0.0M
                 })
                 .ToList();
 
-            var TotalPages = orderList.Count;
-
-            orderList = orderList.Skip(pageNo * 10).Take(10).ToList();
-
-            return new PaginationModel
+            for (int i = 0; i < orderList.Count; i++)
             {
-                TotalPages = CommonHelper.TotalPagesforPagination(TotalPages, 10),
-                CurrentPage = pageNo,
-                Data = orderList
-            };
+                orderList[i].OrderAmount = CalculateOrder(
+                    new GetOrderChargesDTO
+                    {
+                        Defects = _context.OrderDefects.Where(od => od.OrderId == orderList[i].Id).Select(od => od.RepairableDefectId).ToList(),
+                        PriorityId = orderList[i].PriorityId
+                    });
+            }
+
+            var TotalPages = 0;
+
+            if(orderFilters == null)
+            {
+                TotalPages = orderList.Count;
+                orderList = orderList.Skip((pageNo - 1) * 10).Take(10).ToList();
+
+                return new PaginationModel
+                {
+                    TotalPages = CommonHelper.TotalPagesforPagination(TotalPages, 10),
+                    CurrentPage = pageNo,
+                    Data = orderList
+                };
+            }
+            else
+            {
+                // Apply search filter
+                if (!string.IsNullOrEmpty(orderFilters.searchKeyword))
+                {
+                    orderList = orderList.Where(dto =>
+                        dto.Name.Contains(orderFilters.searchKeyword) ||
+                        dto.Email.Contains(orderFilters.searchKeyword) ||
+                        dto.Phone.Contains(orderFilters.searchKeyword) ||
+                        dto.SerialNumber.Contains(orderFilters.searchKeyword) ||
+                        dto.Address.Contains(orderFilters.searchKeyword) ||
+                        dto.BrandName.Contains(orderFilters.searchKeyword) ||
+                        dto.ModelName.Contains(orderFilters.searchKeyword) ||
+                        dto.TechnicianName.Contains(orderFilters.searchKeyword) ||
+                        dto.Id.ToString() == orderFilters.searchKeyword
+                    ).ToList();
+                }
+
+                // Apply Technician Filter
+                if(orderFilters.technicianId != null)
+                {
+                    orderList = orderList.Where(dto => dto.TechnicianId == orderFilters.technicianId).ToList();
+                }
+
+                // Apply Payment Filter
+                if (orderFilters.isPaid != null)
+                {
+                    orderList = orderList.Where(dto => (dto.AmountPaid >= dto.OrderAmount.TotalAmount) == orderFilters.isPaid).ToList();
+                }
+
+                //// Apply Lock state Filter
+                //if (orderFilters.isLocked != null)
+                //{
+                //    orderList = orderList.Where(dto => dto.).ToList();
+                //}
+
+                //// Apply Lock state Filter
+                //if (orderFilters.isLocked != null)
+                //{
+                //    orderList = orderList.Where(dto => dto.).ToList();
+                //}
+
+                // Apply Warranty Filter
+                if (orderFilters.hasWarranty != null)
+                {
+                    orderList = orderList.Where(dto => dto.WarrantyStatus == orderFilters.hasWarranty).ToList();
+                }
+
+                // Apply Status Filter
+                if (orderFilters.statusId != null)
+                {
+                    orderList = orderList.Where(dto => dto.StatusId == orderFilters.statusId).ToList();
+                }
+
+                // Apply Priority Filter
+                if (orderFilters.priorityId != null)
+                {
+                    orderList = orderList.Where(dto => dto.PriorityId == orderFilters.priorityId).ToList();
+                }
+
+                // Apply sorting
+                if (!string.IsNullOrEmpty(orderFilters.sortBy))
+                {
+                    switch (orderFilters.sortBy)
+                    {
+                        case "Status":
+                            orderList = orderFilters.isSortAscending
+                                ? orderList.OrderBy(dto => dto.StatusId).ToList()
+                                : orderList.OrderByDescending(dto => dto.StatusId).ToList();
+                            break;
+                        case "Priority":
+                            orderList = orderFilters.isSortAscending
+                                ? orderList.OrderBy(dto => dto.PriorityId).ToList()
+                                : orderList.OrderByDescending(dto => dto.PriorityId).ToList();
+                            break;
+                        case "Technician":
+                            orderList = orderFilters.isSortAscending
+                                ? orderList.OrderBy(dto => dto.TechnicianId).ToList()
+                                : orderList.OrderByDescending(dto => dto.TechnicianId).ToList();
+                            break;
+                        case "Created at":
+                            orderList = orderFilters.isSortAscending
+                                ? orderList.OrderBy(dto => dto.CreatedAt).ToList()
+                                : orderList.OrderByDescending(dto => dto.CreatedAt).ToList();
+                            break;
+                        case "Order Number":
+                            orderList = orderFilters.isSortAscending
+                                ? orderList.OrderBy(dto => dto.Id).ToList()
+                                : orderList.OrderByDescending(dto => dto.Id).ToList();
+                            break;
+                        //case "Updated At":
+                        //    orderList = orderFilters.isSortAscending
+                        //        ? orderList.OrderBy(dto => dto.UpdatedAt).ToList()
+                        //        : orderList.OrderByDescending(dto => dto.UpdatedAt).ToList();
+                        //    break;
+                        //case "Closed At":
+                        //    orderList = orderFilters.isSortAscending
+                        //        ? orderList.OrderBy(dto => dto.ClosedAt).ToList()
+                        //        : orderList.OrderByDescending(dto => dto.ClosedAt).ToList();
+                        //    break;
+
+                        default:
+                            throw new Exception("Invalid Sort By Value");
+                    }
+                }
+
+                TotalPages = orderList.Count;
+                orderList = orderList.Skip((pageNo - 1) * 10).Take(orderFilters.pageSize).ToList();
+
+                return new PaginationModel
+                {
+                    TotalPages = CommonHelper.TotalPagesforPagination(TotalPages, orderFilters.pageSize),
+                    CurrentPage = pageNo,
+                    Data = orderList
+                };
+            }
         }
         // Private Method
         private void AddTransaction(Transaction transaction)
